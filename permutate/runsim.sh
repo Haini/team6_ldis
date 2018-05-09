4412a373fefb20bd08661ad77ae1426b59eba333
Haini <constantin.schieber@outlook.com>
2018-05-09 10:25:44 +0200

    Reading files does not work

--- permutate/runsim.sh
+++ permutate/runsim.sh
@@ -2,11 +2,12 @@
 
 rm -f *.o *.cf *.vcd
 
-ghdl -a permutate_pkg.vhd
-ghdl -a permutate.vhd
-ghdl -a permutate_tb.vhd
-ghdl -e permutate_tb
-ghdl -r permutate_tb --vcd=wave.vcd --stop-time=10ns
+ghdl -a --std=08 strings_h.vhd
+ghdl -a --std=08 permutate_pkg.vhd
+ghdl -a --std=08 permutate.vhd
+ghdl -a --std=08 permutate_tb.vhd
+ghdl -e --std=08 permutate_tb
+ghdl -r --std=08 permutate_tb --vcd=wave.vcd --stop-time=10ns
 
 rm -f *.o *.cf
 
